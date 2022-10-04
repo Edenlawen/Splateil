04fe63be0afe9676dcf08c24df62aa18bbff6958
Rindra Ramaroson <rindra.ramaroson@ymail.com>
2022-10-04 10:55:44 +0200

    Grille

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+import numpy as np
+import os
+def creerGrille(taille) :
+        grille=np.zeros((taille,taille))
+        return grille
+
+def afficherGrille (grille) :
+        clear = lambda: os.system('cls')
+        clear()
+        print(grille)
+        return 0
+
+a=creerGrille(20)
+afficherGrille(a)
 
